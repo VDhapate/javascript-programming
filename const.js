dc2439e59c12bdf117255db49299448360b75f4a
Vaibhavi Dhapate <vaibhavidhapate721@gmail.com>
2023-07-30 09:59:37 +0530

    const_done

---
+++ const.js
@@ -0,0 +1,10 @@
+let a = "vartik"
+let b = 127
+console.log(a+b)
+console.log(typeof(a+b))
+
+const a1 = { name : "prince",
+             section : 1
+            }
+ console.log(a1)
+ console.log(typeof a1)
